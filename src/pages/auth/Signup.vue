c79c9644a03072dcda96d8efbb026dbfb4e4fb8e
ahmedyasser001 <ahmedyasser7@outlook.com>
2020-10-05 19:31:19 -0700

    project init

--- src/pages/auth/Signup.vue
+++ src/pages/auth/Signup.vue
@@ -2,8 +2,64 @@
 </style>
 
 <template>
-  <div>
-    Register
+  <div class="full-height d-flex justify-center align-center">
+    <div>
+      <router-link to="/">
+        <v-img
+        :src="require('../../assets/logo.png')"
+        max-width="40" max-height="40"
+        to="/"
+        class="mx-auto"
+        >
+        </v-img>
+      </router-link>
+      <v-card
+        width="600"
+        class="my-2"
+      >
+        <v-card-title>Register</v-card-title>
+        <v-card-text>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+            width="100%"
+          >
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="repassword"
+              :rules="repasswordRules"
+              label="Password Confirmation"
+              required
+            ></v-text-field>
+
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="ml-auto"
+              @click="submit"
+            >
+              Submit
+            </v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -11,8 +67,23 @@
 export default {
   data() {
     return {
+        valid: true,
+
         email: '',
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+
         password: '',
+        passwordRules: [
+          v => !!v || 'Password is required',
+        ],
+
+        repassword: '',
+        repasswordRules: [
+          v => !!v || 'Password is required',
+        ],
     };
   },
   // computed: mapState({
@@ -20,9 +91,9 @@ export default {
   mounted() {
   },
   methods: {
-    login() {
+    submit() {
+
     }
   }
-}
+};
 </script>
-
